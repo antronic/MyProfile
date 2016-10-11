69ce6e81ea878aee99b6c84a371bff8f5da42090
antronic <antronic.inc@gmail.com>
2016-10-11 07:33:33 +0700

    finnished page welcome, and profile

--- GulpFile.js
+++ GulpFile.js
@@ -7,15 +7,14 @@ var sourcemaps = require('gulp-sourcemaps');
 var imagemin = require('gulp-imagemin');
 
 var sass_input = './src/sass/*.+(scss|sass)';
-var bootstrap = './src/bootstrap-sass/assets/stylesheets/bootstrap.scss';
-var all_boostrap = './src/bootstrap-sass/assets/stylesheets/**/**/*.+(scss|sass)';
 var css_output = './public/assets/css/';
 var view_dir = './views/*.+(ejs)';
 
-var img_src = './src/img/*.+(jpg|png|gif|jpeg|svg)';
+var img_src = './src/img/**.+(jpg|png|gif|jpeg|svg)';
+var img_src_dir = './src/img/';
 var img_dest = './public/img/';
 
-var port = 5000;
+var port = 9000;
 var ip = '127.0.0.1';
 var proxy = ip+":"+port;
 
@@ -31,7 +30,6 @@ gulp.task('watch', function(){
     });
 
     gulp.watch(sass_input, ['sass']);
-    gulp.watch(bootstrap, ['bootstrap-sass']);
     gulp.watch(img_src, ['img_min']);
     gulp.watch(view_dir).on('change', browserSync.reload);
     gulp.watch(img_dest).on('change', browserSync.reload);
@@ -40,20 +38,6 @@ gulp.task('watch', function(){
 gulp.task('sass', function(){
     return gulp
         .src(sass_input)
-        .pipe(sourcemaps.init())
-        .pipe(sass(sass_options).on('error', sass.logError))
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(minifycss())
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest(css_output))
-        .pipe(browserSync.stream());
-});
-
-gulp.task('bootstrap-sass', function(){
-    return gulp
-        .src(bootstrap)
         .pipe(sourcemaps.init())
         .pipe(sass(sass_options).on('error', sass.logError))
         .pipe(rename({
--- src/server.js
+++ src/server.js
@@ -1,2 +1,21 @@
+let project_name = "MyProfile"
 import express from 'express'
 let app = express()
+import http from 'http'
+let server = http.createServer(app)
+import path from 'path'
+import bodyParser from 'body-parser'
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+import routes from './app/routes'
+routes(app)
+
+server.listen(9000 | process.env.PORT, () => {
+  console.log(project_name + " is listening on " + server.address().port)
+})
