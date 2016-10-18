854e2f422c3ed28d0fdbfbc9bd3fd0b1c05462b2
antronic <antronic.inc@gmail.com>
2016-10-19 03:34:25 +0700

    change some point for ESLint's rules

--- GulpFile.js
+++ GulpFile.js
@@ -1,39 +1,39 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var rename = require('gulp-rename');
-var minifycss = require('gulp-minify-css');
-var browserSync = require('browser-sync');
-var sourcemaps = require('gulp-sourcemaps');
-var imagemin = require('gulp-imagemin');
+var gulp = require('gulp')
+var sass = require('gulp-sass')
+var rename = require('gulp-rename')
+var minifycss = require('gulp-minify-css')
+var browserSync = require('browser-sync')
+var sourcemaps = require('gulp-sourcemaps')
+var imagemin = require('gulp-imagemin')
 
-var sass_input = './src/sass/*.+(scss|sass)';
-var css_output = './public/assets/css/';
-var view_dir = './views/*.+(ejs)';
+var sass_input = './src/sass/*.+(scss|sass)'
+var css_output = './public/assets/css/'
+var view_dir = './views/*.+(ejs)'
 
-var img_src = './src/img/**.+(jpg|png|gif|jpeg|svg)';
-var img_src_dir = './src/img/';
-var img_dest = './public/img/';
+var img_src = './src/img/**.+(jpg|png|gif|jpeg|svg)'
 
-var port = 9000;
-var ip = '127.0.0.1';
-var proxy = ip+":"+port;
+var img_dest = './public/img/'
+
+var port = 9000
+var ip = '127.0.0.1'
+var proxy = ip+':'+port
 
 var sass_options = {
     errLogToConsole: true,
     outputStyle: 'expanded'
-};
+}
 
 gulp.task('watch', function(){
 
     browserSync.init({
         proxy: proxy
-    });
+    })
 
-    gulp.watch(sass_input, ['sass']);
-    gulp.watch(img_src, ['img_min']);
-    gulp.watch(view_dir).on('change', browserSync.reload);
-    gulp.watch(img_dest).on('change', browserSync.reload);
-});
+    gulp.watch(sass_input, ['sass'])
+    gulp.watch(img_src, ['img_min'])
+    gulp.watch(view_dir).on('change', browserSync.reload)
+    gulp.watch(img_dest).on('change', browserSync.reload)
+})
 
 gulp.task('sass', function(){
     return gulp
@@ -46,12 +46,12 @@ gulp.task('sass', function(){
         .pipe(minifycss())
         .pipe(sourcemaps.write())
         .pipe(gulp.dest(css_output))
-        .pipe(browserSync.stream());
-});
+        .pipe(browserSync.stream())
+})
 
 gulp.task('img_min', function(){
     return gulp
         .src(img_src, {base: './src/img/*'})
         .pipe(imagemin())
-        .pipe(gulp.dest(img_dest));
-});
+        .pipe(gulp.dest(img_dest))
+})
