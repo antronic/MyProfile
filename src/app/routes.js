347348d3840bb4b48865112086998822bc698b3a
antronic <antronic.inc@gmail.com>
2016-10-21 15:35:26 +0700

    add more debug

--- src/app/routes.js
+++ src/app/routes.js
@@ -2,6 +2,8 @@ export default (app) => {
 
   app.get('/', (req, res)=>{
     res.render('index')
+    var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+    console.log(fullUrl)
   })
 
   app.get('/clock', (req, res)=>{
